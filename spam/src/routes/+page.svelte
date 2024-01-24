fd9e3321073979e809586dab306491d0274a5048
sambok23 <a24746440@gmail.com>
2024-01-24 23:37:50 +0900

    init

---
+++ spam/src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<div class="wrap"> 
+    <div class="sence">
+        <h1>이 웹사이트는 이분들의 후원으로 만드러 졌습니드ㅏ</h1>
+        <ul>
+            <div class="item">
+                <li>
+                    <h2>🌟김마유🌟</h2>
+                </li>
+                <li>정현서</li>
+                <li>전재민</li>
+                <li>이경민</li>
+                <li>김민재</li>
+                <li>김채운</li>
+                <li>정주호</li>
+                <li>국재윤</li>
+                <li>최유빈</li>
+                <li>정윤환</li>
+            </div>
+        </ul>
+    </div>
+</div>
+
+<style>
+	.wrap {
+        display: grid;
+        place-items: center;
+		position: absolute;
+		inset: 0;
+		background: radial-gradient(hsl(220 14% 20%), hsl(220 20% 10%));
+	}
+
+    .sence {
+        display: grid;
+        place-items: center;
+    }
+
+    .item{
+        display: grid;
+        place-items: center;
+    }
+
+    h1, li, h2 {
+        color: white;
+    }
+</style>
